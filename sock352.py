6f52ad5d0f2eb2b787fcf2daecda07ca6e6bbb1d
modi160497 <30540253+modi160497@users.noreply.github.com>
2019-03-07 22:36:00 -0500

    changes to connect and accept

--- sock352.py
+++ sock352.py
@@ -1,6 +1,6 @@
 
 import binascii
-import socket as syssock
+import socket 
 import struct
 import sys
 
@@ -31,12 +31,37 @@ class socket:
     def connect(self,address):  # fill in your code here 
 		server_address = (address, UDP_port)
 		self.sock.connect(server_address)
+		sock352PktHdrData = '!BBBBHHLLQQLL'
+		udpPkt_hdr_data = struct.Struct(sock352PktHdrData)
+		version =  1
+		flags = 1
+		opt_ptr = 0
+		checksum = 0
+		source_port = 0
+		dest_port= 0
+		sequence_no = 1
+		ack_no = 0
+		window = 0
+		payload_len = 0
+		
+		header = udpPkt_header_data.pack(version, flags, opt_ptr, protocol, checksum, source_port, dest_port, sequence_no, ack_no, window, payload_len)
+		
+		self.sock.sendto(header, server_address)
+		
         return 
     
     def listen(self,backlog):
         return
 
     def accept(self):
+	
+		conn, addr = self.sock.accept()
+		with conn:
+			print('Connected by', addr)
+			while True:
+				data = conn.recv(1024)
+				header_unpack = udpPkt_header_data.unpack('!BBBBHHLLQQLL',header)
+		
         (clientsocket, address) = (1,1)  # change this to your code 
         return (clientsocket,address)
     
