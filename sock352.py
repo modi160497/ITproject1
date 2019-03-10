5cd69c60ac7b559608e169e75a5f7e23d346ea3c
modi160497 <30540253+modi160497@users.noreply.github.com>
2019-03-09 19:48:33 -0500

    Update sock352.py

--- sock352.py
+++ sock352.py
@@ -1,132 +1,145 @@
-#!/usr/bin/python
 
-# This is the CS 352 Spring 2017 Client for the 1st programming
-# project
- 
+import binascii
+import socket as syssock 
+import sys
+import threading
+import struct
 
-import argparse
-import time
-import struct 
-import md5
-import os 
-import sock352
 
-def main():
-    # parse all the arguments to the client 
-    parser = argparse.ArgumentParser(description='CS 352 Socket Client')
-    parser.add_argument('-f','--filename', help='File to Send', required=False)
-    parser.add_argument('-d','--destination', help='Destination IP Host', required=True)
-    parser.add_argument('-p','--port', help='remote sock352 port', required=False)
-    parser.add_argument('-u','--udpportRx', help='UDP port to use for receiving', required=True)
-    parser.add_argument('-v','--udpportTx', help='UDP port to use for sending', required=False)
+# these functions are global to the class and
+# define the UDP ports all messages are sent
+# and received from
 
-    # get the arguments into local variables 
-    args = vars(parser.parse_args())
-    filename = args['filename']
-    destination = args['destination']
-    udpportRx = args['udpportRx']
 
-    if (args['udpportTx']):
-        udpportTx = args['udpportTx']
-    else:
-        udpportTx = ''
-        
-    # the port is not used in part 1 assignment, except as a placeholder
-    if (args['port']): 
-        port = args['port']
-    else:
-        port = 1111 
+version =  1
+flags = 1
+opt_ptr = 0
+checksum = 0
+source_port = 0
+dest_port= 0
+sequence_no = 1
+ack_no = 0
+window = 0
+payload_len = 0
+protocol = 0
+sock352PktHdrData = '!BBBBHHLLQQLL'
+UDP_port = 0
+UDP_IP = 0
+header_len = 00001100
+udpPkt_hdr_data = struct.Struct(sock352PktHdrData)
 
-    # open the file for reading
-    if (filename):
-        try: 
-            filesize = os.path.getsize(filename)
-            fd = open(filename, "rb")
-            usefile = True
-        except:
-            print ( "error opening file: %s" % (filename))
-            exit(-1)
-    else:
-        pass 
+def init(UDPportTx,UDPportRx):   # initialize your UDP socket here 
+	global UDP_port
+	global UDP_IP
+	UDP_port = UDPportTx
+	UDP_IP = UDPportRx
 
-    # This is where we set the transmit and receive
-    # ports the client uses for the underlying UDP
-    # sockets. If we are running the client and
-    # server on the same machine, these ports
-    # need to be different. If they are running on
-    # different machines, we can re-use the same
-    # ports. 
-    if (udpportTx):
-        sock352.init(udpportTx,udpportRx)
-    else:
-        sock352.init(udpportRx,udpportRx)
+	print(UDP_IP)
+	print(UDP_port)
+	pass
+	
+sock = None
 
-    # create a socket and connect to the remote server
-    s = sock352.socket()
-    s.connect((destination,port))
-    
-    # send the size of the file as a 4 byte integer
-    # to the server, so it knows how much to read
-    # FRAGMENTSIZE = 8192
-    longPacker = struct.Struct("!L")
-    fileLenPacked = longPacker.pack(filesize)
-    s.send(fileLenPacked)
+class socket:
+	
+	def __init__(self):  # fill in your code here 
+	
+		if sock is None:
+			self.sock = syssock.socket(
+				syssock.AF_INET, syssock.SOCK_DGRAM)
+			#self.sock.setblocking(0)
+			self.sock.settimeout(1) 
+		else:
+			self.sock = sock
+			self.sock.settimeout(1) 
+			#self.sock.setblocking(0)
+		return
+	
+	def bind(self,address):
+		return 
 
-    # use the MD5 hash algorithm to validate all the data is correct
-    # mdhash = md5.new()
+	def connect(self,address):  # fill in your code here
 
-    # loop for the size of the file, sending the fragments 
-    bytes_to_send = filesize
+		#first step of handshake
 
-    start_stamp = time.clock()    
-    # while (bytes_to_send > 0):
-    #     fragment = fd.read(FRAGMENTSIZE)
-    #     mdhash.update(fragment)
-    #     totalsent = 0
-    #     # make sure we sent the whole fragment 
-    #     while (totalsent < len(fragment)):
-    #         sent = s.send(fragment[totalsent:])
-    #         if (sent == 0):
-    #             raise RuntimeError("socket broken")
-    #         totalsent = totalsent + sent
-    #     bytes_to_send = bytes_to_send - len(fragment)
-    file_contents = fd.read()
-    # mdhash.update(file_contents)
-    totalsent = 0
-    # make sure we sent the whole fragment 
-    while (totalsent < len(file_contents)):
-        sent = s.send(file_contents)
-        if (sent == 0):
-            raise RuntimeError("socket broken")    
-    end_stamp = time.clock() 
-    lapsed_seconds = end_stamp - start_stamp
-    
-    # this part send the lenght of the digest, then the
-    # digest. It will be check on the server 
-    
-    # digest = mdhash.digest()
-    # # send the length of the digest
-    # long = len(digest)
-    # digestLenPacked = longPacker.pack(long)
-    # sent = s.send(digestLenPacked)
-    # if (sent != 4):
-    #     raise RuntimeError("socket broken")
-    
-    # # send the digest 
-    # sent = s.send(digest)
-    # if (sent != len(digest)):
-    #     raise RuntimeError("socket broken")
+		udpPkt_header_data = struct.Struct(sock352PktHdrData)
+		
+		header = udpPkt_header_data.pack(version, flags, opt_ptr, protocol, checksum, header_len,source_port, dest_port, sequence_no, ack_no, window, payload_len)
+		print(type(header))
+		self.sock.sendto(header, address)
+		
+		#look for connections 
+		#the sender sends a packet, 3rd step of handshake
 
-    if (lapsed_seconds > 0.0):
-        print ("client1: sent %d bytes in %0.6f seconds, %0.6f MB/s " % (filesize, lapsed_seconds,
-(filesize/lapsed_seconds)/(1024*1024)))
-    else:
-        print ("client1: sent %d bytes in %d seconds, inf MB/s " % (filesize, lapsed_seconds))        
+		sock_recv = syssock.socket(
+				syssock.AF_INET, syssock.SOCK_DGRAM)
 
-    fd.close()
-    s.close()
-# this gives a main function in Python
-if __name__ == "__main__":
-    main()
+		sock_recv.bind(('', int(UDP_port)))
+		(data,address) = sock_recv.recv(1024)
+		
+		header_unpack = udpPkt_header_data.unpack('!BBBBHHLLQQLL',header)
+		array = header_unpack.split(', ')
+
+		unpack_list=header_unpack.split(', ')
+		ack_rec = unpack_list(8)
+		seq = unpack_list(7)
+		if(ack_rec > 1):
+			print("hello from client")
+			ack_send = seq + 1
+			seq = ack_rec
+			
+			udpPkt_header_data = struct.Struct(sock352PktHdrData)
+
+			header = udpPkt_header_data.pack(version, flags, opt_ptr, protocol, checksum, header_len, source_port, dest_port, seq, ack_send, window, payload_len)
+
+			self.sock.sendto(header, address)
+		else:
+			return 
+		#no packet is received 
+		
+			
+	
+	def listen(self,backlog):
+		return
+
+	def accept(self):
+
+		#2nd step of handshake
+		self.sock.bind(('',UDP_port))
+		
+		(data,address) = self.sock.recv(1024)
+		header_unpack = udpPkt_header_data.unpack('!BBBBHHLLQQLL',header)
+
+		array = header_unpack.split(', ')
+
+		unpack_list=header_unpack.split(', ')
+		flag=unpack_list(1)
+		if flag is 1:
+			print("hello from server")
+			seq=unpack_list(6)
+			ack_no = seq + 1
+			
+			udpPkt_header_data = struct.Struct(sock352PktHdrData)
+
+			header = udpPkt_header_data.pack(version, flags, opt_ptr, protocol, checksum, header_len, source_port, dest_port, sequence_no, ack_no, window, payload_len)
+			self.sock.sendto(header, address)
+				
+		
+	   # change this to your code 
+		return (self.sock,address)
+	
+	def close(self):   # fill in your code here 
+		return 
+
+	def send(self,buffer):
+		bytessent = 0     # fill in your code here 
+		return bytesent 
+
+	def recv(self,nbytes):
+		bytesreceived = 0     # fill in your code here
+		return bytesreceived 
+
+
+	
 
 
